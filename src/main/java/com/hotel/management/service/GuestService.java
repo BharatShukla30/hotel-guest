a7856020af9f32967eb8fb2114379e8eda531372
Parmesh Kumar <parmeshkumar414@gmail.com>
2023-10-23 23:43:39 +0530

    fix: Duplicate entry of existing guest

--- src/main/java/com/hotel/management/service/GuestService.java
+++ src/main/java/com/hotel/management/service/GuestService.java
@@ -1,6 +1,7 @@
 package com.hotel.management.service;
 
 import com.hotel.management.entity.Guest;
+import org.postgresql.util.PSQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import java.util.Optional;
 
 public interface GuestService {
 
-    public Guest addGuest(Guest guest);
+    public Guest addGuest(Guest guest) throws RuntimeException;
 
     public Optional<Guest> getByAadhaarNumber(String aadhaarNumber);
 
